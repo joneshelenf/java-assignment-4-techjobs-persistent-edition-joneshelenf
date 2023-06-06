ee07f75396532c58ad9b39ad4915cbf695ae522f
Helen Jones <helenfrancesjones@gmail.com>
2023-06-06 14:02:32 -0500

    update job model class

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,23 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Entity
-public class Job{
+public class Job extends AbstractEntity {
 
-    @Id
-    @GeneratedValue
-    private int id;
+    @ManyToOne
+    private Employer employer;
 
-    private String name;
+    @ManyToMany
+    @NotNull(message = "Choose a skill to continue.")
+    private List<Skill> skills;
 
-    private String employer;
-    private String skills;
+    public Job() {} //no arg constructor!!!!!!!!!!!!!!!
 
-    public Job() {
-    }
+//    @Id
+//    @GeneratedValue
+//    private int id;
+//    private String name;
+//    private String employer;
+//    private String skills;
 
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer anEmployer, List<Skill> someSkills) {
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
@@ -25,27 +31,30 @@ public class Job{
 
     // Getters and setters.
 
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public Employer getEmployer() {
 
-    public String getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
+
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
+
         return skills;
     }
 
-    public void setSkills(String skills) {
+    public void setSkills(List<Skill> skills) {
+
         this.skills = skills;
     }
 }
