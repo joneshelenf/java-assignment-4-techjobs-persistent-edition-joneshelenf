310d7c41769abc88d89c9c3e5e95d28448dd2f24
Helen Jones <helenfrancesjones@gmail.com>
2023-06-07 13:47:31 -0500

    finished up to part 3

--- queries.sql
+++ queries.sql
@@ -1,12 +1,20 @@
 -- Part 1: Test it with SQL: list the columns and their data types in the table as a SQL comment.
 SELECT *
-FROM job
+FROM job;
 
--- Part 2: Test it with SQL: write a query to list the names of the employers in St. Louis City. Do NOT specify an ordering for the query results.
+-- Part 2: Test it with SQL: write a query to list the names of the employers in St. Louis City.
+--Do NOT specify an ordering for the query results.
 SELECT name
 FROM employer
 WHERE location = "St. Louis City";
 
--- Part 3: Test it with SQL
-DROP TABLE job
--- Part 4: Test it with SQL
+-- Part 3: Test it with SQL remove job table
+DROP TABLE job;
+
+-- Part 4: Test it with SQL write a query to return the names of all skills that are attached to jobs in alphabetical order.
+--If a skill does not have a job listed, it should not be included in the results of this query
+SELECT *
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,14 +1,16 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
-
-    @NotBlank(message = "Enter a location.")
     @Size(min = 3, max = 100, message = "Must be between 3 and 100 characters long." )
     private String location;
 
@@ -19,7 +21,10 @@ public class Employer extends AbstractEntity {
         this.location = location;
     }
 
-    //no arg constructor??????????
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
+
     public Employer() { };
 
 }
