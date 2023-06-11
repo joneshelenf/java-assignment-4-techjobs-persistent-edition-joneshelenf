6dd4c5575ad70aab8e6a2a3de65aedd1dba586ad
Helen Jones <helenfrancesjones@gmail.com>
2023-06-11 11:02:02 -0500

    SQL error fixed on bootRun

--- queries.sql
+++ queries.sql
@@ -15,7 +15,13 @@ DROP TABLE job;
 
 -- Part 4: Test it with SQL write a query to return the names of all skills that are attached to jobs in alphabetical order.
 --If a skill does not have a job listed, it should not be included in the results of this query
+--SELECT *
+--FROM skill
+--ORDER BY name ASC;
+
 SELECT *
 FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
 
